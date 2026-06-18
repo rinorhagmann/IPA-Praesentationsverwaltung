86b9771b1bec0976d441ed8d846894b6565baad2
Rinor Hagmann <rinorhagmann@outlook.com>
2026-06-18 10:06:30 +0200

    initial commit

---
+++ IPA-Praesentationsverwaltung/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IPA_Praesentationsverwaltung.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
